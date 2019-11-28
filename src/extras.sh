d6cd363a01c1aafbddd350cb71db4beea1e0ed52
Chris McCormick <chris@mccormick.cx>
2019-11-28 15:02:57 +0800

    Added str-split for single character splits.

--- src/extras.sh
+++ src/extras.sh
@@ -6,3 +6,20 @@ _str_replace () {
 }
 
 _fref "str-replace" _str_replace
+
+_str_split() {
+  local var="${ANON["${1}"]}"; shift
+  local ifs="${ANON["${1}"]}"; shift
+  _list
+  local newlist="${r}"
+
+  while IFS="${ifs}" read -ra _str_split_arr; do
+        for _str_split_itm in "${_str_split_arr[@]}"; do
+          _string "${_str_split_itm}";
+          _conj! "${newlist}" "${r}";
+        done
+  done <<< "${var}"
+  r="${newlist}"
+}
+
+_fref "str-split" _str_split
