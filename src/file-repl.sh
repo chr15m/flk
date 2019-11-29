85938e7384bf7a61d0998f3ca0dc82fdd820ebda
Chris McCormick <chris@mccormick.cx>
2019-11-29 09:26:45 +0800

    Fix lack of errors in non-interactive mode. #3

--- src/file-repl.sh
+++ src/file-repl.sh
@@ -1,8 +1,9 @@
 # load/run file from command line (then exit)
 if [[ "${1}" ]]; then
     REP "(load-file \"${1}\")"
-    exit 0
+    [ "${r}" = "nil" ] && exit 0 || { echo "${r}"; exit 127; };
 fi
+
 
 # repl loop
 REP "(println (str \"Fleck\"))"
