1c2d36ff921eefbba2e417c829a95596c2ddd296
Chris McCormick <chris@mccormick.cx>
2019-12-02 15:59:19 +0800

    Updated ip addr example.

--- examples/ip-addresses.clj
+++ examples/ip-addresses.clj
@@ -1,3 +1,3 @@
 (let [cmd "ip addr | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'"
-      ips (str-split (str-replace (sh* cmd) "\\\\n" "\n") "\n")]
+      ips (str-split (sh* cmd) "\n")]
   (println (pr-str {:ips ips})))
