771ad80bc89a00ed1d41ffbb48576be5031f0a3a
Chris McCormick <chris@mccormick.cx>
2019-12-03 17:44:15 +0800

    Don't run REPL or load-file when flk is sourced. #10

--- src/file-repl.sh
+++ src/file-repl.sh
@@ -1,13 +1,17 @@
-# load/run file from command line (then exit)
-if [[ "${1}" ]]; then
-    REP "(load-file-without-hashbang \"${1}\")"
-    [ "${r}" = "nil" ] && exit 0 || { echo "${r}"; exit 127; };
+( return 0 2>/dev/null ) && _fleck_sourced=1 || _fleck_sourced=0
+
+if [ "${_fleck_sourced}" = "0" ]
+then
+  # load/run file from command line (then exit)
+  if [[ "${1}" ]]; then
+      REP "(load-file-without-hashbang \"${1}\")"
+      [ "${r}" = "nil" ] && exit 0 || { echo "${r}"; exit 127; };
+  fi
+
+  # repl loop
+  REP "(println (str \"Fleck\"))"
+  while true; do
+      READLINE "user> " || exit "$?"
+      [[ "${r}" ]] && REP "${r}" && echo "${r}"
+  done
 fi
-
-
-# repl loop
-REP "(println (str \"Fleck\"))"
-while true; do
-    READLINE "user> " || exit "$?"
-    [[ "${r}" ]] && REP "${r}" && echo "${r}"
-done
