38db7f58b60d093df6a437e0bf957c014f9994be
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-12-19 16:37:10 -0300

    test: coverage all utils functions

--- tests/lang-utils.clj
+++ tests/lang-utils.clj
@@ -45,3 +45,108 @@
 
   (testing "true list mixed"
     (is (= (and 1 true "ok") "ok"))))
+
+(def m (hashmap-list {:a 1 :b 2 :c 3}))
+
+(deftest "hashmap-list"
+  (testing "return sequence"
+    (is (= (sequential? m) true)))
+
+  (testing "return vector tuples"
+    (is (= (map (fn [x] (count x)) m) (list 2 2 2))))
+
+  (testing "key match"
+    (is (= (first (first m)) (keyword "c"))))
+
+  (testing "value match"
+    (is (= (last (first m)) 3))))
+
+(deftest "merge"
+  (testing "regular merge"
+    (is (= (merge {:a 1} {:b 2}) {:a 1 :b 2})))
+
+  (testing "replace merge"
+    (is (= (merge {:a 1} {:a 2}) {:a 2}))))
+
+(deftest "key-name"
+  (testing "return key as string"
+    (is (= (key-name :hello) "hello")))
+
+  (testing "bypass value if a not keyworkd"
+    (is (= (key-name 1) 1))))
+
+(defn foo [x] (str x))
+
+(deftest "call"
+  (testing "return function from string"
+    (is (= (fn? (call "foo")) true)))
+
+  (testing "spawn call"
+    (is (= ((call "foo") "x") "x"))))
+
+(deftest "callable?"
+  (testing "check if function exist"
+    (is (= (callable? "foo") true)))
+
+  (testing "check if function not exist"
+    (is (= (callable? "bar") false))))
+
+(def m {:a 1 :b {:x 2} :c {:x {:y 3}}})
+
+(deftest "get-in"
+  (testing "flat values"
+    (is (= (get-in m [:a]) 1)))
+
+  (testing "nested values"
+    (is (= (get-in m [:c :x :y]) 3)))
+
+  (testing "not found nested values"
+    (is (= (get-in m [:c :x :z]) nil))))
+
+(deftest "even?"
+  (testing "positive"
+    (is (= (even? 2) true)))
+
+  (testing "zero"
+    (is (= (even? 0) true)))
+
+  (testing "negative"
+    (is (= (even? -2) true)))
+
+  (testing "odd"
+    (is (= (even? 1) false))))
+
+(deftest "odd?"
+  (testing "positive"
+    (is (= (odd? 1) true)))
+
+  (testing "zero"
+    (is (= (odd? 0) false)))
+
+  (testing "negative"
+    (is (= (odd? -1) true)))
+
+  (testing "even"
+    (is (= (odd? 2) false))))
+
+(def f (str "/tmp/lang-utils-" (time-ms)))
+
+(deftest "spit"
+
+  (testing "create a file"
+    (is (= (spit f "hello world!") nil)))
+
+  (testing "checking file"
+    (is (= (slurp f) "hello world!")))
+
+  (testing "rewrite a file"
+    (is (= (spit f "rewrite!") nil)))
+
+  (testing "checking file rewrited"
+    (is (= (slurp f) "rewrite!")))
+
+  (testing "append a file"
+    (is (= (spit f "append!" {:append true}) nil)))
+
+  (testing "checking file rewrited"
+    (is (= (slurp f) "rewrite!append!"))))
