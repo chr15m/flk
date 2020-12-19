9a4d7685d47a0bbbf023ea5a690e02ce10cdc7a3
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-12-19 18:30:21 -0300

    test: coverage new flk functions

--- tests/patches.clj
+++ tests/patches.clj
@@ -14,15 +14,14 @@
     (is (= (range 5 0 -1) (list 5 4 3 2 1))))
 
   (testing "negative list"
-  (is (= (range -5 1) (list -5 -4 -3 -2 -1 0)))))
-
+    (is (= (range -5 1) (list -5 -4 -3 -2 -1 0)))))
 
 (deftest "mod"
   (testing "even"
     (is (= (mod 10 2) 0)))
 
   (testing "four"
-   (is (= (mod 10 6) 4))))
+    (is (= (mod 10 6) 4))))
 
 (deftest "file-exists"
   (testing "exists: /etc/hosts"
@@ -62,3 +61,86 @@
   (testing "parallel fib"
     (is (= (pmap (fn [x] (fib x)) (range 30)) fc))))
 
+(deftest "str-join"
+  (testing "add separator, csv"
+    (is (= (str-join "," "banana" "apples" "orange") "banana,apples,orange")))
+
+  (testing "add separator, '; '"
+    (is (= (str-join "; " "banana" "apples" "orange") "banana; apples; orange"))))
+
+(def s "fleck!")
+
+(deftest "str-subs"
+  (testing "char 0 to 3"
+    (is (= (str-subs s 0 3) "fle")))
+
+  (testing "char 3 to 5"
+    (is (= (str-subs s 3 5) "ck!")))
+
+  (testing "from char 3"
+    (is (= (str-subs s 3) "ck!")))
+
+  (testing "reverse"
+    (is (= (str-subs s -2) "k!"))))
+
+(deftest "str-indent"
+  (testing "indent with 2 spaces (default)"
+    (is (= (str-indent s) "  fleck!")))
+
+  (testing "indent with 1 spaces"
+    (is (= (str-indent s 1) " fleck!")))
+
+  (testing "indent with 4 spaces"
+    (is (= (str-indent s 4) "    fleck!"))))
+
+(def f (str "/tmp/patches-" (time-ms)))
+
+(deftest "file-write"
+
+  (testing "create a file"
+    (is (= (file-write f "hello world!") nil)))
+
+  (testing "checking file"
+    (is (= (slurp f) "hello world!")))
+
+  (testing "rewrite a file"
+    (is (= (file-write f "rewrite!") nil)))
+
+  (testing "checking file rewrited"
+    (is (= (slurp f) "rewrite!")))
+
+  (testing "append a file"
+    (is (= (file-write f "append!" true) nil)))
+
+  (testing "checking file rewrited"
+    (is (= (slurp f) "rewrite!append!"))))
+
+(deftest "unlink"
+  (testing "remove file"
+    (is (= (unlink f) nil)))
+
+  (testing "check if file was removed"
+    (is (= (file-exists? f) false))))
+
+(deftest "unset"
+  (testing "set var __FLECK"
+    (is (= (env "__FLECK" "fleck!") "fleck!")))
+
+  (testing "unset var __FLECK"
+    (is (= (unset "__FLECK") nil)))
+
+  (testing "check if var not exist"
+    (is (= (env "__FLECK") nil))))
+
+(def f (str "/tmp/patches-trap-" (time-ms)))
+
+(deftest "trap!"
+  (testing (str "touch a file at: " f)
+    (is (= (sequential? (trap! (str "{ touch " f " ; }") "EXIT")) true))))
+
+(deftest "println-stderr"
+  (testing "print on stderror"
+    (is (= (println-stderr "hello world!") nil))))
+
+    ; there is no way to test exit without exit :(
+(exit! 0)
